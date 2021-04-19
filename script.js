b062c10d40ae0b4d96534e86774b8fdaa5c9ac9b
antonio <macbookpro@Dudes-MacBook-Pro.local>
2021-04-19 17:31:24 -0400

    made star/snow like particles

--- script.js
+++ script.js
@@ -21,13 +21,32 @@ const scene = new THREE.Scene()
 const textureLoader = new THREE.TextureLoader()
 
 /**
- * Test cube
+ * Particles
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+// Geometry
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 500
+const positions = new Float32Array(count * 3)
+
+for(let i = 0; i < count * 3; i++)
+{
+    positions[i] = (Math.random() - 0.5) * 10
+}
+
+particlesGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(positions, 3)
 )
-scene.add(cube)
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.02,
+    sizeAttenuation: true
+})
+//Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+
+scene.add(particles)
 
 /**
  * Sizes
